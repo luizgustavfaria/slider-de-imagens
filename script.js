1552f082fd4f4ea069807e6655657f32122ec125
luizgustavfaria <99811863+luizgustavfaria@users.noreply.github.com>
2022-02-17 13:04:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let contador = 1;
+
+setInterval(function () {
+  document.getElementById("slide" + contador).checked = true;
+  contador++;
+  if (contador > 5) {
+    contador = 1;
+  }
+}, 3500);
